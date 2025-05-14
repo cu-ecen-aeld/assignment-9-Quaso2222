4ad13cf135fc13f89735ba3dbe2deef49a994718
quaso2222 <quaso2222@github.com>
2025-05-14 16:58:19 +0800

    add aesdsocket

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -16,15 +16,19 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app 
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer  $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/aesdsocket   $(TARGET_DIR)/usr/bin
 	mkdir -p $(TARGET_DIR)/usr/conf/
+	mkdir -p $(TARGET_DIR)/etc/init.d/S99aesdsocket
 	cp -dpR $(@D)/conf/* $(TARGET_DIR)/usr/conf/
 	cp $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
 	cp $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
+	cp $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
